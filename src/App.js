5ff6a150978e7b31a88023155e1b7cf8bed45c4a
Arvind Nagalbone <arvindn@inboxops.local>
2023-09-25 11:51:15 +0530

    Optimize filter code

--- src/App.js
+++ src/App.js
@@ -6,26 +6,25 @@ import Card from "./components/Card";
 import restList from "./utils/mockData";
 const AppLayout = () => {
   const [restuarant,setRestuarant] = useState(restList);
-  const [search,setSearch] = useState("");
 
   const filterSearchResto = (e)=>{
     const rest = e.target.value;
-    const filterRes = restList.filter((res)=>{
-      return res.info.name.toLowerCase().includes(rest.toLowerCase())
-    });
+    const filterRes = restList.filter((res)=>res.info.name.toLowerCase().includes(rest.toLowerCase()));
     setRestuarant(filterRes);
-
   }
+
+  const filterTopRatedResto = ()=>{
+    let filterResto = restuarant.filter((res)=> res.info.avgRating > 4.5);
+    setRestuarant(filterResto);
+  }
+
   return (
     <>
       <Header />
       <div className="heading">
       <input type="text" onChange={filterSearchResto} placeholder="Search Reataurant Near You.."/>&nbsp;&nbsp;
       <button onClick={()=>setRestuarant(restList)}>All</button>&nbsp;&nbsp;
-      <button onClick={()=>{
-        let filterResto = restuarant.filter((res)=> res.info.avgRating > 4.5);
-        setRestuarant(filterResto);
-      }}>Top Rated Restaurants</button>
+      <button onClick={filterTopRatedResto}>Top Rated Restaurants</button>
       
       </div>
       <div className="card-container">
